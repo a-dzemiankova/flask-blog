94df4734ab2d6c5de31e4297c2814f03d6c63591
Anastasia Demenkova <a.piatrovskaya@gmail.com>
2025-01-21 14:42:25 +0300

    fix: fixed the app structure and minor bugs

---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+migrate = Migrate()
+login_manager = LoginManager()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+
+    db.init_app(app)
+    migrate.init_app(app)
+    login_manager.init_app(app)
+
+    login_manager.login_view = 'login'
+
+    from .routes import init_routes
+    init_routes(app)
+
+    return app
+
+@login_manager.user_loader
+def load_user(user_id):
+    from .models import User
+    return User.query.get(int(user_id))
